4d5e7eede17c2fa191b7589157da35793c493b22
_minhhien_ <128836350+hienbeparanoid@users.noreply.github.com>
2023-06-25 09:35:19 +0700

    Update customer_gender.py

--- customer_gender.py
+++ customer_gender.py
@@ -10,15 +10,23 @@ customers = pd.read_csv('./data/Dim_customer.csv')
 gender_count = customers['gender'].value_counts()
 male_count = gender_count.get('M',0)
 female_count = gender_count.get('F',0)
+notspecified_count = gender_count.get('Not Specified',0)
+
+Gender = ['M','F','Not Specified']
+Count = [male_count,female_count,notspecified_count]
 
 #draw a bar chart
 plt.figure(figsize=(6,6))
-plt.bar(['M','F'], [male_count,female_count], color=['#F09B59','#1783DB'])
+plt.bar(Gender, Count, color=['#F09B59','#1783DB','#BC9DC7'])
 
 #label each axis
-plt.xlabel('Giới tính')
-plt.ylabel('Số lượng')
-plt.title('Số lượng khách hàng theo giới tính')
+plt.xlabel('Giới tính', fontsize=10, fontweight='bold')
+plt.ylabel('Số lượng', fontsize=10, fontweight='bold')
+plt.title('Số lượng khách hàng theo giới tính', fontsize=16, fontweight='bold')
+
+#show the value of each bar
+for i, value in enumerate(Count):
+    plt.text(i, value, str(value), ha='center', va='bottom', fontweight='bold', fontsize=9)
 
 plt.show()
 
